69dfa22083725cdd56f5752abad3571c2c521482
Brock Huskisson <huskissonbrock@gmail.com>
2026-04-19 17:39:25 -0600

    move currently data to JSON file

--- frontend/src/components/Currently.jsx
+++ frontend/src/components/Currently.jsx
@@ -1,11 +1,4 @@
-/** Rows shown in the hero “Currently” block. */
-export const currentlyRows = [
-  { key: 'Building', value: 'Personal site on a home server' },
-  { key: 'Reading', value: 'Project Hail Mary' },
-  { key: 'Watching', value: 'Breaking Bad' },
-  { key: 'Listening', value: 'Sam Barber - Restless Mind' },
-  { key: 'Suffering', value: 'New York Jets fan' },
-]
+import currentlyRows from '../data/currently.json'
 
 export default function CurrentlyPanel() {
   return (

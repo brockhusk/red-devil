4233c629c25bdacf44476217ace8297601795444
Brock Huskisson <huskissonbrock@gmail.com>
2026-04-19 20:00:38 -0600

    add RUM to APM trace correlation

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,26 @@
+import { datadogRum } from '@datadog/browser-rum';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+datadogRum.init({
+    applicationId: '52f05f07-2887-4191-ac25-bb362fc6d36d',
+    clientToken: 'pub9f4dffb6a8b7c1c84a2308f96c15018f',
+    site: 'datadoghq.com',
+    service: 'red-devil',
+    env: 'production',
+    // version: '1.0.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+    allowedTracingUrls: [
+      { match: "https://brockhusk.com", propagatorTypes: ["datadog"] }
+  ],
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
